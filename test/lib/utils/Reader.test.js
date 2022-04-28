8e3977b4d370c52e9440722dd665a8c2c57771b7
Andres Guevara <=>
2022-04-27 23:18:49 -0500

    Se actualiza el test de la clase Reader

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -4,7 +4,7 @@ const path = require('path')
 describe("Test for Reader class",()=>{
     test('Return a list of explorers',()=>{        
         
-        const explorers = Reader.readJsonFile(path.join(__dirname,'../../data/explorers.json'))   
+        const explorers = Reader.readJsonFile('test/data/explorers.json')   
         const isArray = Array.isArray(explorers)
 
         expect(isArray).toBe(true)
