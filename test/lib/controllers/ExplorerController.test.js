a12ef929572c6b7d9c5efba8662d4fc215b97587
Andres Guevara <=>
2022-04-28 23:10:21 -0500

    se agrega getExplorersUsernamesByMission y getExplorersAmonutByMission a ExplorerController

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -2,10 +2,32 @@ const ExplorerController = require("../../../lib/controllers/ExplorerController"
 
 describe('Test for ExplorerController class',()=>{
     test('get explorers by mission',()=>{        
-        const explorersFilteredByMission = ExplorerController.getExplorersByMission('node')
+        const explorersInNodeMission = ExplorerController.getExplorersByMission('node')
 
-        expect(explorersFilteredByMission.length).toBe(10)
+        expect(explorersInNodeMission.length).toBe(10)
     })
 
-   
+    test("get explorers usernames by mission",()=>{
+        const explorersUsernameInNode = ExplorerController.getExplorersUsernamesByMission('node')
+        const explorersNamesExpected = [
+            "ajolonauta1",
+            "ajolonauta2",
+            "ajolonauta3",
+            "ajolonauta4",
+            "ajolonauta5",
+            "ajolonauta11",
+            "ajolonauta12",
+            "ajolonauta13",
+            "ajolonauta14",
+            "ajolonauta15"
+        ]
+
+        expect(explorersUsernameInNode).toEqual(explorersNamesExpected)
+    })
+
+    test('get explorers amonut By Mission',()=>{
+        const amountExplorersInNode = ExplorerController.getExplorersAmonutByMission('node')        
+        
+        expect(amountExplorersInNode).toBe(10)
+    })
 })
